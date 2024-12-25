f10e06af61e6b92bc1121c4d75a9f07f16929b7a
Mackenzie Moore <mmoore@Mackenzies-MacBook-Pro.local>
2024-12-24 19:21:41 -0600

    Imported items for main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/styles.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
