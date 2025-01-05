bae099be12af3d1df5a9e6967d2dafe6744f5b34
mackemo <mackenzielmoore14@gmail.com>
2025-01-05 17:22:48 -0600

    Included pages needed for app

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,44 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/styles.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from './App';
+import Error from './pages/Error';
+import About from './pages/About';
+import Resume from './pages/Resume';
+import Positions from './pages/Positions';
+import Headshots from './pages/Headshots';
+import Contact from './pages/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: "/Resume",
+        element: <Resume />,
+      },
+      {
+        path: "/Positions",
+        element: <Positions />,
+      },
+      {
+        path: "/Headshots",
+        element: <Headshots />,
+      },
+      {
+        path: "/Contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
