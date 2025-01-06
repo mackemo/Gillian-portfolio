0cc38c83ebdc9ee99f9b2e7b20085c94433dc4f6
mackemo <mackenzielmoore14@gmail.com>
2025-01-05 18:36:24 -0600

    Completed About me bio and inserted professional headshot

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,20 +7,19 @@ export default function About() {
                 <div>
                     <div className="d-flex flex-wrap align-items-center">
                         <div className="intro col-md-6 d-flex flex-column">
-                            <h2 className="fs-3 fw-normal fst-italic">Theater Actress</h2>
+                            <h2 className="fs-3 fw-normal fst-italic">Actor, Singer, Designer</h2>
                             <p className="fs-6">
-                                My journey in theater began through extensive training and performances, where I developed my skills and received 
-                                recognition for my passion and dedication to the stage. This accomplishment showcases my unwavering commitment 
-                                and drive for excellence in acting!
+                                Murmurs from the house, a last minute prop check, and...
                                 <br></br>
                                 <br></br>
-                                I previously performed with a respected theater troupe in Babylon as an Actress. During my time there, I portrayed a variety of roles, 
-                                demonstrating my ability to bring diverse characters to life. At present, I continue to perform in productions, where I immerse myself 
-                                in the story and explore complex emotional depth within each role.
+                                My name is Gillian Flatt and I am an Indianapolis based actor, singer, and costume designer. Throughout the past four years I have 
+                                been cultivating my love for storytelling and pushing myself to learn new tools in order to tell those stories. I hold immense value 
+                                in trying to find new angles and aspects of theatre as we all lift each other up as artists. Outside of theatre I enjoy writing, 
+                                drawing, and showing off pictures of my french bulldog, Kevin! Please feel free to take some time to peruse my past acting and 
+                                costume design projects, and I hope to work with you soon ◡̈
                                 <br></br>
                                 <br></br>
-                                Holding a Bachelor's degree in Performing Arts from the University, I bring a meticulous and creative approach to every character. 
-                                I am passionate about pushing my boundaries and perfecting my craft to deliver memorable and compelling performances in every show!
+                                ...Lights down, curtain up, and away we go!
                             </p> 
                         </div>
                         
