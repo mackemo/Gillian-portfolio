9f6d160847a0219d3e1337bbb195c2ed2d0e609d
mackemo <mackenzielmoore14@gmail.com>
2025-04-04 20:37:16 -0500

    Included resume and headshot on resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,12 +1,25 @@
+import headshot from '../assets/images/headshot.png'
+import resume from '../assets/images/resume.png'
+
 export default function Resume() {
-    return (
-      <section className="container col-md-10" id="skills">
-        <div className="row"></div>
-          <h3 className="title fw-medium">Resume</h3>
-          <div className="d-flex flex-wrap justify-content-center">
-            <div className="text-center m-4"></div>
-          </div>
-      </section>
-    );
-  }
-  
+  return (
+    <section className="container col-md-12" id="skills">
+      <div className="row">
+        <h3 className="title fw-medium">Resume</h3>
+        <div>
+            <div className="d-flex flex-wrap align-items-center">
+                <div className="col-md-6 d-flex justify-content-center">
+                    <img className="img-resume col-md-12" src={headshot} alt="Gillian Resume"/>
+                </div>
+                
+                <div className="col-md-6 d-flex justify-content-center">
+                    <img className="img-gillian col-md-12" src={resume} alt="Gillian Flatt"/>
+                </div>
+            </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+
