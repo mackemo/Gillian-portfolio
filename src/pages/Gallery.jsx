35df383372832272519adfbdd5710385cf6621f1
mackemo <mackenzielmoore14@gmail.com>
2025-04-04 20:55:16 -0500

    Added images for Gallery page

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,11 +1,63 @@
-export default function Positions() {
+import images from '../assets/images'
+
+export default function Gallery() {
     return (
-      <section className="container col-md-10" id="skills">
-        <div className="row"></div>
-          <h3 className="title fw-medium">Gallery</h3>
-          <div className="d-flex flex-wrap justify-content-center">
-            <div className="text-center m-4"></div>
-          </div>
-      </section>
+        <section className="container col-md-10" id="projects">
+            <div className="row">
+                <div>
+                    <h3 className="title fw-medium">Gallery</h3>
+                    <div className="work-topics">
+                        <div className="work">
+                            <a href="" target="_blank">
+                                <img src={images.theydont} alt="They Don't Pay? We Won't Pay!"/>
+                                <div className="info">
+                                <h5>They Don't Pay? We Won't Pay!</h5>
+                                </div>
+                            </a>
+                        </div>
+                        <div className="work">
+                            <a href="" target="_blank">
+                                <img src={images.radium} alt="Radium Girls"/>
+                                <div className="info">
+                                <h5>Radium Girls</h5>
+                                </div>
+                            </a>
+                        </div>
+                        <div className="work">
+                            <a href="" target="_blank">
+                                <img src={images.men} alt="Men on Boats"/>
+                                <div className="info">
+                                <h5>Men on Boats</h5>
+                                </div>
+                            </a>
+                        </div>
+                        <div className="work">
+                            <a href="" target="_blank">
+                                <img src={images.love} alt="About Love"/>
+                                <div className="info">
+                                <h5>About Love</h5>
+                                </div>
+                            </a>
+                        </div>
+                        <div className="work">
+                            <a href="" target="_blank">
+                                <img src={images.costume} alt="Specific Costume Work"/>
+                                <div className="info">
+                                <h5>Specific Costume Work</h5>
+                                </div>
+                            </a>
+                        </div>
+                        <div className="work">
+                            <a href="" target="_blank">
+                                <img src={images.wonderland} alt="Wonderland"/>
+                                <div className="info">
+                                <h5>Wonderland</h5>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
-  }
+} 
